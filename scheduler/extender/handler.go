8594ff623545449e8074233dac5ce722a8423c5b
Boris Mattijssen <borismattijssen@gmail.com>
2016-06-13 14:06:37 +0200

    Address PR comments

--- scheduler/extender/handler.go
+++ scheduler/extender/handler.go
@@ -5,17 +5,17 @@ import (
 	"fmt"
 	"net/http"
 
-	"k8s.io/kubernetes/pkg/api"
-	schedulerapi "k8s.io/kubernetes/plugin/pkg/scheduler/api"
+	k8sApi "k8s.io/kubernetes/pkg/api"
+	k8sSchedulerApi "k8s.io/kubernetes/plugin/pkg/scheduler/api"
 )
 
-// handler handles a request by the kubernetes scheduler.
+// handler handles a request from the kubernetes scheduler.
 // handler receives a list of nodes and a pod and returns the node with the
 // lowest joules label.
 func handler(w http.ResponseWriter, r *http.Request) {
 	// decode request body.
 	dec := json.NewDecoder(r.Body)
-	received := &schedulerapi.ExtenderArgs{}
+	received := &k8sSchedulerApi.ExtenderArgs{}
 	err := dec.Decode(received)
 	if err != nil {
 		fmt.Printf("Error when trying to decode response body to struct: %v\n", err)
@@ -26,19 +26,19 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 	// select the node to schedule on.
 	node, err := selectNode(&received.Nodes)
-	var items []api.Node
+	var items []k8sApi.Node
 	if err != nil {
 		fmt.Printf("Encountered error when selecting node: %v", err)
 	} else {
-		items = []api.Node{*node}
+		items = []k8sApi.Node{*node}
 	}
 
 	// return the result.
 	enc := json.NewEncoder(w)
 	w.Header().Set("Content-Type", "application/json; charset=UTF-8")
 	w.WriteHeader(http.StatusOK)
-	enc.Encode(&schedulerapi.ExtenderFilterResult{
-		Nodes: api.NodeList{
+	enc.Encode(&k8sSchedulerApi.ExtenderFilterResult{
+		Nodes: k8sApi.NodeList{
 			Items: items,
 		},
 	})
